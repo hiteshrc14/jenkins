b13421850eebf6cd39f4f7db447d90421b2bb639
Hiteshkumar Choudhary <hiteshrc14@gmail.com>
2023-09-01 20:21:31 +0530

    Update editor.groovy

--- editor.groovy
+++ editor.groovy
@@ -1,31 +1,35 @@
-import jenkins.model.Jenkins
+mport jenkins.model.Jenkins
 
-// Define the Multi-Branch Pipeline project name as a parameter
+// Define the Multi-Branch Pipeline name as a parameter
 def multiBranchPipelineName = 'YourMultiBranchPipelineName'
 
 // Get the Jenkins instance
 def jenkins = Jenkins.instance
 
 // Find the Multi-Branch Pipeline project by name
-def multiBranchProject = jenkins.getItem(multiBranchPipelineName)
+def multiBranchProject = jenkins.items.find { it ->
+    it.displayName == multiBranchPipelineName && it instanceof org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
+}
 
 if (multiBranchProject) {
+    // List existing branch names in the source code repository
+    def existingBranches = [:]
+    
+    // You should customize this logic to fetch the list of existing branches from your repository
+    // Here, we assume you have a function called getExistingBranches() that returns a list of existing branch names
+    existingBranches = getExistingBranches() // Replace with your logic
+    
     // Iterate through all branch jobs within the Multi-Branch Pipeline project
     multiBranchProject.getItems().each { branchJob ->
-        branchJob.getBuilds().each { build ->
-            def branchName = build.getDisplayName()
-
-            // Check if the branch still exists in the source code repository
-            // You should customize this logic based on how you determine if a branch is deleted
-            boolean branchExists = true  // Replace with your logic
-
-            if (!branchExists) {
-                // Delete the build for the deleted branch
-                println("Deleting build for branch: ${branchName}")
-                build.delete()
-            }
+        def branchName = branchJob.name
+        
+        // Check if the branch still exists in the source code repository
+        if (!existingBranches.contains(branchName)) {
+            // Delete the job for the deleted branch
+            println("Deleting job for branch: ${branchName}")
+            branchJob.delete()
         }
     }
 } else {
-    println("Multi-Branch Pipeline project '${multiBranchPipelineName}' not found.")
+    println("Multi-Branch Pipeline project '${multiBranchPipelineName}' not found or is not a WorkflowMultiBranchProject.")
 }
