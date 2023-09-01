aebe1972524419db7ad06ea9ca8fee6f123097b2
Hiteshkumar Choudhary <hiteshrc14@gmail.com>
2023-09-01 20:14:52 +0530

    Update editor.groovy

--- editor.groovy
+++ editor.groovy
@@ -1,17 +1,31 @@
-import jenkins.branch.MultiBranchProject
 import jenkins.model.Jenkins
+
+// Define the Multi-Branch Pipeline project name as a parameter
+def multiBranchPipelineName = 'YourMultiBranchPipelineName'
 
 // Get the Jenkins instance
 def jenkins = Jenkins.instance
 
-// List all Multi-Branch Pipeline projects
-def multiBranchProjects = jenkins.getAllItems(MultiBranchProject.class)
+// Find the Multi-Branch Pipeline project by name
+def multiBranchProject = jenkins.getItem(multiBranchPipelineName)
 
-if (multiBranchProjects) {
-    println("List of Multi-Branch Pipeline projects:")
-    multiBranchProjects.each { project ->
-        println("- ${project.displayName} (${project.name})")
+if (multiBranchProject) {
+    // Iterate through all branch jobs within the Multi-Branch Pipeline project
+    multiBranchProject.getItems().each { branchJob ->
+        branchJob.getBuilds().each { build ->
+            def branchName = build.getDisplayName()
+
+            // Check if the branch still exists in the source code repository
+            // You should customize this logic based on how you determine if a branch is deleted
+            boolean branchExists = true  // Replace with your logic
+
+            if (!branchExists) {
+                // Delete the build for the deleted branch
+                println("Deleting build for branch: ${branchName}")
+                build.delete()
+            }
+        }
     }
 } else {
-    println("No Multi-Branch Pipeline projects found.")
+    println("Multi-Branch Pipeline project '${multiBranchPipelineName}' not found.")
 }
